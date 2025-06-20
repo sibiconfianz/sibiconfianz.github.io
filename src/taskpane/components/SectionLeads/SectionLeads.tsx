916281e52d13b2c3d7088a754541c6d1698d4850
sibi <sibi@confianzit.com>
2025-06-20 18:34:45 +0530

    updated.

--- src/taskpane/components/SectionLeads/SectionLeads.tsx
+++ src/taskpane/components/SectionLeads/SectionLeads.tsx
@@ -16,7 +16,7 @@ type LeadSectionProps = {
     canCreatePartner: boolean;
     canCreateLead: boolean;
 //    exstingLeads: [];
-    opportunityLeads: [];  // Add this property to the type
+    opportunityLeads: Lead[];  // Add this property to the type
 
 };
 
