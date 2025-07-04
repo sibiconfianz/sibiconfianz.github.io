04db1e71f32e235ae654aca526e0d70d790794ba
sibi <sibi@confianzit.com>
2025-07-04 11:08:38 +0530

    updated.

--- src/taskpane/components/CollapseSection/CollapseSection.tsx
+++ src/taskpane/components/CollapseSection/CollapseSection.tsx
@@ -36,8 +36,6 @@ class CollapseSection extends React.Component<CollapseSectionProps, CollapseSect
     };
 
     render() {
-//        const showSearchButton = this.props.className;
-//        console.log('================showSearchButton', showSearchButton)
 
         const searchButton = this.props.showSearchButton && (
             <FontAwesomeIcon icon={faSearch} className="collapse-section-button" onClick={this.props.onSearchButtonClick} />
