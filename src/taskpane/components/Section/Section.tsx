35fbad3df67322c34becd99bb65bc41bd1236957
sibi <sibi@confianzit.com>
2025-07-24 00:54:16 +0530

    updated.

--- src/taskpane/components/Section/Section.tsx
+++ src/taskpane/components/Section/Section.tsx
@@ -67,8 +67,8 @@ class Section extends React.Component<SectionAbstractProps, SectionAbstractState
             // Remove the history and only log the most recent message.
             const message = result.value.split('<div id="x_appendonsend"></div>')[0];
             const subject = Office.context.mailbox.item.subject;
-            const isCompose = typeof item.from?.getAsync === 'function';
-            console.log('---------------------------------------------------------p', message, subject, isCompose)
+//            const isCompose = typeof item.from?.getAsync === 'function';
+            console.log('---------------------------------------------------------p', message, subject)
             const requestJson = Object.assign(
                 {
                     partner_id: this.props.partner.id,
